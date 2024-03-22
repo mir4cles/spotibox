b10bf8a8643856319a6c3863466985005978e9b6
juri miralles <mir4cles88@gmail.com>
2024-03-22 13:20:51 +0100

    feat: query audio features and render data

--- src/pages/Library.tsx
+++ src/pages/Library.tsx
@@ -1,5 +1,6 @@
 import {
 	Scopes,
+	type AudioFeatures,
 	type Page,
 	type SavedTrack,
 	type SpotifyApi,
@@ -19,31 +20,56 @@ import { useSpotify } from "../hooks/UseSpotify";
 
 const Library = ({ sdk }: { sdk: SpotifyApi }) => {
 	const [savedTracks, setSavedTracks] = useState<Page<SavedTrack>>();
+	const [audioFeatures, setAudioFeatures] = useState<Array<AudioFeatures>>();
+
+	console.log({ savedTracks, audioFeatures });
 
 	useEffect(() => {
 		// eslint-disable-next-line @typescript-eslint/no-floating-promises
 		(async () => {
-			const savedTracksResults = await sdk.currentUser.tracks.savedTracks();
+			const savedTracksResults = await sdk.currentUser.tracks.savedTracks(
+				50,
+				50
+			);
 			setSavedTracks(savedTracksResults);
 		})();
 	}, [sdk]);
 
-	if (!savedTracks) {
+	useEffect(() => {
+		// eslint-disable-next-line @typescript-eslint/no-floating-promises
+		(async () => {
+			if (savedTracks) {
+				const trackIds = savedTracks.items.map((item) => item.track.id);
+				const audioFeatures = await sdk.tracks.audioFeatures(trackIds);
+				setAudioFeatures(audioFeatures);
+			}
+		})();
+	}, [savedTracks, sdk.tracks]);
+
+	if (!savedTracks || !audioFeatures) {
 		return <Fragment>Loading...</Fragment>;
 	}
 
 	return (
 		<Table>
-			<TableCaption>Spotify Search for The Beatles</TableCaption>
+			<TableCaption>Saved tracks</TableCaption>
 			<TableHeader>
 				<TableRow>
 					<TableHead className="text-left">Name</TableHead>
 					<TableHead>Artists</TableHead>
 					<TableHead className="text-right">Popularity</TableHead>
+					<TableHead className="text-right">Danceability</TableHead>
+					<TableHead className="text-right">Key</TableHead>
+					<TableHead className="text-right">Tempo</TableHead>
+					<TableHead className="text-right">Accousticness</TableHead>
+					<TableHead className="text-right">Valence</TableHead>
 				</TableRow>
 			</TableHeader>
 			<TableBody>
-				{savedTracks?.items.map((item) => {
+				{savedTracks.items.map((item) => {
+					const audioFeature = audioFeatures.find(
+						(feature) => feature.id === item.track.id
+					);
 					return (
 						<TableRow key={item.track.id}>
 							<TableCell className="text-left">{item.track.name}</TableCell>
@@ -52,6 +78,25 @@ const Library = ({ sdk }: { sdk: SpotifyApi }) => {
 							</TableCell>
 							<TableCell className="text-right">
 								{item.track.popularity}
+							</TableCell>
+							<TableCell className="text-right">
+								{audioFeature?.danceability.toLocaleString("en", {
+									style: "percent",
+								})}
+							</TableCell>
+							<TableCell className="text-right">{audioFeature?.key}</TableCell>
+							<TableCell className="text-right">
+								{audioFeature?.tempo}
+							</TableCell>
+							<TableCell className="text-right">
+								{audioFeature?.acousticness.toLocaleString("en", {
+									style: "percent",
+								})}
+							</TableCell>
+							<TableCell className="text-right">
+								{audioFeature?.valence.toLocaleString("en", {
+									style: "percent",
+								})}
 							</TableCell>
 						</TableRow>
 					);
